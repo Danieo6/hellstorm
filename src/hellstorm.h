7ebb819a406a3e5788c345a5e9424b8acbc717d2
Daniel <daniel.budzinski0@gmail.com>
2025-05-17 19:57:12 +0200

    Initial commit

---
+++ src/hellstorm.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <godot_cpp/classes/object.hpp>
+
+using namespace godot;
+
+class HellStorm : public Object {
+	GDCLASS(HellStorm, Object);
+
+	protected:
+		static void _bind_methods();
+
+	public:
+		void test();
+
+		HellStorm();
+};
